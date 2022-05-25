d32927abf7670c1898b1ea669a9ce146295e1cdc
Guyrlam <guyrlamsilvaduque@gmail.com>
2022-05-25 16:10:37 -0300

    feat: first commit

---
+++ carousel.js
@@ -0,0 +1,24 @@
+// CARROSSEL
+let ulsElis = document.querySelectorAll('ul li')
+
+let lista1 = document.querySelectorAll('#lista li')
+
+function show(indice, indiceLista) {
+
+    let listaUl = document.querySelector('#lista')
+
+    let mover  = 295.3
+    let posicaoXDireita = mover
+    let posicaoXEsquerda = -mover
+
+    if(indice == +1) {
+        //console.log('Mover para direita')
+        listaUl.scrollBy(posicaoXDireita, 0)
+    }
+    if(indice == -1) {
+        //console.log('Mover para esquerda')
+        listaUl.scrollBy(posicaoXEsquerda, 0)
+    }
+
+}
+
